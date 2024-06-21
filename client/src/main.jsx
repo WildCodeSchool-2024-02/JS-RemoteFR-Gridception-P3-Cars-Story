a27dcaf1f8948a18c424a8e0a90cfaede4fd1351
Benoît Vandanjon <benoit@vandanjon.fr>
2024-06-21 10:46:59 +0200

    add style + routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,24 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import Home from "./pages/Home";
+import SportsCars from "./pages/SportsCars";
+
+import "./styles/style.css";
 
 const router = createBrowserRouter([
   {
-    path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/sports-cars",
+        element: <SportsCars />,
+      },
+    ],
   },
 ]);
 
