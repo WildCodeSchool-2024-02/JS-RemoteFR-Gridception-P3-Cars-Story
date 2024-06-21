d0d0b43a4efba57de5228b253973bb013f1a4f06
Benoît Vandanjon <benoit@vandanjon.fr>
2024-06-21 10:07:13 +0200

    add necessary dependencies

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,55 +1,45 @@
 create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+    id int unsigned primary key auto_increment not null,
+    email varchar(255) not null unique,
+    password varchar(255) not null
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
+CREATE TABLE `Brand` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
 );
 
-
-CREATE TABLE IF NOT EXISTS `Brand` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `Fuel_Type` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `Fuel_Type` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `recharging` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `cars_id` int(11) NOT NULL,
+    `date_time_start` datetime NOT NULL,
+    `date_time_end` datetime NOT NULL,
+    `kilowatt_hour` decimal(10, 2) NOT NULL,
+    `cost` decimal(10, 2) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `cars_id` (`cars_id`),
+    CONSTRAINT `recharging_ibfk_1` FOREIGN KEY (`cars_id`) REFERENCES `cars` (`id`)
 );
 
-
-CREATE TABLE IF NOT EXISTS `recharging` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cars_id` int(11) NOT NULL,
-  `date_time_start` datetime NOT NULL,
-  `date_time_end` datetime NOT NULL,
-  `kilowatt_hour` decimal(10,2) NOT NULL,
-  `cost` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `cars_id` (`cars_id`),
-  CONSTRAINT `recharging_ibfk_1` FOREIGN KEY (`cars_id`) REFERENCES `cars` (`id`)
-);
-
-
-CREATE TABLE IF NOT EXISTS `cars` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `Model` varchar(255) NOT NULL,
-  `year` int(11) NOT NULL,
-  `mileage` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
-  `picture` varchar(255) DEFAULT NULL,
-  `brand_id` int(11) NOT NULL,
-  `fuel_type_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `brand_id` (`brand_id`),
-  KEY `fuel_type_id` (`fuel_type_id`),
-  CONSTRAINT `cars_ibfk_1` FOREIGN KEY (`brand_id`) REFERENCES `brand` (`id`),
-  CONSTRAINT `cars_ibfk_2` FOREIGN KEY (`fuel_type_id`) REFERENCES `fuel_type` (`id`)
+CREATE TABLE `cars` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `Model` varchar(255) NOT NULL,
+    `year` int(11) NOT NULL,
+    `mileage` int(11) NOT NULL,
+    `price` int(11) NOT NULL,
+    `picture` varchar(255) DEFAULT NULL,
+    `brand_id` int(11) NOT NULL,
+    `fuel_type_id` int(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `brand_id` (`brand_id`),
+    KEY `fuel_type_id` (`fuel_type_id`),
+    CONSTRAINT `cars_ibfk_1` FOREIGN KEY (`brand_id`) REFERENCES `brand` (`id`),
+    CONSTRAINT `cars_ibfk_2` FOREIGN KEY (`fuel_type_id`) REFERENCES `fuel_type` (`id`)
 );
