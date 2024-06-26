4eea8b983db8be3c39da8b1f3aa1ca2d5b8da17a
hamid <hbo711772@gmail.com>
2024-06-26 15:19:38 +0200

    change prettier

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,6 @@ import OldandNewCars from "./pages/OldandNewCars";
 import LifeOfCars from "./pages/LifeOfCars";
 import Chatelectriccars from "./pages/Chatelectriccars";
 
-
 import "./styles/style.css";
 import LifeOfCars from "./pages/Life-Of-Cars";
 
@@ -27,20 +26,18 @@ const router = createBrowserRouter([
         element: <SportsCars />,
       },
       {
-
-        path:"/Old-and-New-Cars",
-        element: <OldandNewCars />
+        path: "/Old-and-New-Cars",
+        element: <OldandNewCars />,
       },
       {
-        path:"/Life-Of-Cars",
-        element: <LifeOfCars />
+        path: "/Life-Of-Cars",
+        element: <LifeOfCars />,
       },
       {
-        path:"/Chat-electric-cars",
-        element: <Chatelectriccars />
+        path: "/Chat-electric-cars",
+        element: <Chatelectriccars />,
       },
     ],
-
   },
 ]);
 
--- client/src/pages/OldandNewCars.jsx
+++ client/src/pages/OldandNewCars.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 
-
 export default function oldandNewCars() {
   return (
     <div id="Old-and-New-Cars-Container">
